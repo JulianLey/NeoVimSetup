587990a5322c4bfaee84dbf99283576a84af39a5
Julian Ley <contact@julianley.com>
2024-07-14 23:22:43 +0200

    Update packer.lua

--- nvim/lua/Julian/packer.lua
+++ nvim/lua/Julian/packer.lua
@@ -1,31 +1,17 @@
--- packer.lua
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Ensure packer is installed
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
-end
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
--- Initialize packer
-packer.init {
-  -- Packer settings
-}
-
--- Define plugins
-packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  -- Add other plugins here
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use { "rose-pine/neovim", as = "rose-pine" }
 end)
 
