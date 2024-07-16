8497bacaa73820d2260f6a7e1fe7bfb0ba6c6e02
JulianLey <146943186+JulianLey@users.noreply.github.com>
2024-07-16 23:26:24 +0200

    Updated to latest version

--- nvim/lua/Julian/set.lua
+++ nvim/lua/Julian/set.lua
@@ -4,9 +4,12 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-
+vim.o.autoindent = true
 vim.o.smartindent = true
+
 vim.o.wrap = false
+vim.o.cursorline = true
+
 
 vim.o.swapfile = true
 vim.o.backup = true
@@ -14,8 +17,30 @@ vim.o.backupdir = os.getenv("USERPROFILE") .. "/.vim/backupdir"
 vim.o.undodir = os.getenv("USERPROFILE") .. "/.vim/undodir"
 vim.o.undofile = true
 
-vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.hlsearch = true
 vim.o.incsearch = true
+
+-- Clear search highlights
+vim.keymap.set('n', '<CR>', ':noh<CR><CR>', { noremap = true, silent = true })
+
+-- Insert mode mappings for pairs
+vim.keymap.set('i', "'", "''<left>", { noremap = true })
+vim.keymap.set('i', '"', '""<left>', { noremap = true })
+vim.keymap.set('i', '(', '()<left>', { noremap = true })
+vim.keymap.set('i', '[', '[]<left>', { noremap = true })
+vim.keymap.set('i', '{', '{}<left>', { noremap = true })
+
+-- Insert mode mappings for tabs
+vim.keymap.set('i', '\t\'', "''", { noremap = true })
+vim.keymap.set('i', '\t"', '""', { noremap = true })
+vim.keymap.set('i', '\t(', '()', { noremap = true })
+vim.keymap.set('i', '\t[', '[]', { noremap = true })
+vim.keymap.set('i', '\t{', '{}', { noremap = true })
+vim.keymap.set('i', 'nei', '<Esc>', { noremap = true, silent = true })
+
+-- Insert mode mapping for new line braces
+vim.keymap.set('i', '{<CR>', '{<CR>}<ESC>O', { noremap = true })
 
 vim.o.termguicolors = true
 
@@ -26,36 +51,5 @@ vim.o.updatetime = 50
 
 vim.o.colorcolumn = "82"
 vim.g.mapleader = " "
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+vim.o.clipboard = "unnamedplus"
 
